40ba854de0508954908ecf43e03fd3c2bd17345f
Michael Lowe <mrlowememphis@yahoo.com>
2020-03-12 20:17:19 -0500

    MVP met all test working

--- src/utils/formatSeasons.js
+++ src/utils/formatSeasons.js
@@ -10,18 +10,3 @@ export const formatSeasons = allEpisodes => {
   // console.log(seasons);
   return seasons;
 };
-
-
-
-
-// export const formatSeasons = allEpisodes => {
-//   const seasons = {};
-//   allEpisodes.forEach(e => {
-//     if (!seasons.hasOwnProperty(`Season ${e.season}`)) {
-//       seasons[`Season ${e.season}`] = [];
-//     }
-//     seasons[`Season ${e.season}`].push(e);
-//   });
-//   console.log(seasons);
-//   return seasons;
-// };
