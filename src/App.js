485d510cab07bd16d63f7aa8af24dfb69d87c19a
mgomri <mel77gomri@gmail.com>
2020-04-20 16:38:25 -0400

    put fetchShows into /api

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+import { fetchShow } from './api/fetchShow';
 
 export default function App() {
   const [show, setShow] = useState(null);
@@ -15,17 +16,12 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow()
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
+  
   }, []);
 
   const handleSelect = e => {
---
+++ src/api/fetchShow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from "axios"
+export const fetchShow = () => {
+   return axios.get(
+       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+       )
+       .then(res => res)
+
+      
+  };
