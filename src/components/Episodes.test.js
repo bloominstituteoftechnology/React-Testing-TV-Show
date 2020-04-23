a3b27076d55de317f93b3e44f9c98512395227e6
Veto Ramirez <vetorobert3@gmail.com>
2020-04-22 20:35:51 -0700

    Created a test for rendering a component.

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Episodes from './Episodes';
+
+test('render Episode component', () => {
+  render(<Episodes episodes={[]} />)
+})
