3bac317fee3dd251420bc73922ff1bfdfa265714
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-04-23 20:46:15 -0700

    MPV??

--- src/App.test.js
+++ src/App.test.js
@@ -10,39 +10,55 @@ console.log(mockFetchShow);
 
 test('fetches and renders the data', async () => {
     const mockData = {
-            data:{
-                name:"devin",
-                image:{original: '/original'},
-                summary:'<p>A Love letter<p>',
-                _embedded:{
-                    episodes:[
-                        {
-                            id:101,
-                            name:"chapter11",
-                            runtime:100,
-                            season:100,
-                            number:11,
-                            image:{ medium:''},
-                            summary:'<p> just watch it</p>',
-        
-                        },
-                    ],
-                }
-        
-                
-            }
-        }
+        data: {
+          name: 'devin',
+          image: { original: '/original' },
+          summary: '<p>A love letter<p>',
+          _embedded: {
+            episodes: [
+              {
+                id: 101,
+                name: 'chapter 11',
+                runtime: 100,
+                season: 100,
+                number: 11,
+                image: { medium: '' },
+                summary: '<p>just watch it</p>',
+              },
+            ],
+          },
+        },
+      };
+      jest.mock('react-dropdown', () => ({ options, value, onChange }) => {
+        return (
+          <select
+            data-testid='select'
+            value={value}
+            onChange={onChange}
+          >
+            {options.map((option) => (
+              <option key={Date.now()} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        );
+      });
+      it('fetches and renders the data', async () => {
+        mockFetchShow.mockResolvedValueOnce(mockData);
+        const { getByTestId } = render(<App />);
+        expect(getByTestId('fetching')).toBeInTheDocument();
+        await waitForElementToBeRemoved(() => getByTestId('fetching'));
+        const select = getByTestId('select');
+        fireEvent.change(select, { target: {value: 99 }});
+        await waitFor(() => {
+          expect(getByTestId('episode')).toBeInTheDocument()
+        })
+      });
 
-    mockFetchShow.mockResolvedValueOnce(mockData);
-    const { getByTestId } = render(<App />);
 
-    expect(getByTestId('fetching data')).toBeInTheDocument();
-    await waitForElementToBeRemoved(() => getByTestId('fetching'));
-    const select = getByTestId('select');
-    fireEvent.change(select, { target: {value: 99 }});
-    await waitFor(() => {
-      expect(getByTestId('episode')).toBeInTheDocument()
-    })
+
+
   });
 
 
