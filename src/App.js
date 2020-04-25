ff58e2b7fd76ff78472d738315a616688ae8a78d
unknown <Dustin.Guillen14@yahoo.com>
2020-04-24 20:45:14 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import { fetchShow } from './api/fetchShow';
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -15,17 +15,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
+    fetchShow()
+      .then(res => {
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
@@ -38,10 +32,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <h2>Ello Poppet</h2>
       <img className="poster-img" src={show.image.original} alt={show.name} />
       <h1>{show.name}</h1>
-      
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
---
+++ src/api/fetchShow.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const fetchShow = (props) => {
+    return axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+        console.log(res);
+        return res;
+      });
+  };
