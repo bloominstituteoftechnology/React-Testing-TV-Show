556094a9eecd300788833162bf312755df3754f4
lex-marie790 <lexi.marie790@gmail.com>
2020-06-03 16:55:18 -0700

    moved async call, created files for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
+import { fetchShow } from './api/fetchShow';
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -15,17 +16,18 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    // const fetchShow = () => {
+      // axios
+      //   .get(
+      //     "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      //   )
+      fetchShow()
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
+    // };
+    // fetchShow();
   }, []);
 
   const handleSelect = e => {
