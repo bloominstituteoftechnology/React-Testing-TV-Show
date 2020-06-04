7e308b5b34315a2abcdafafd84209a677c11ac99
Brian Clayton <finalb@projectfinalboss.com>
2020-06-04 09:45:13 -0400

    moved fetchShow

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, {useEffect, useState} from "react";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
-import { formatSeasons } from "./utils/formatSeasons";
+import {fetchShow} from './api/fetchShow';
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
 export default function App() {
+
+
+
+
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
+  
+  
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow
+      .then(res => {
+        setShow(res.data);
+        setSeasons(formatSeasons(res.data._embedded.episodes));
+      }
   }, []);
 
   const handleSelect = e => {
---
+++ src/api/fetchShow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+export const fetchShow = () => {
+      axios
+        .get(
+          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
+        .then(res => {
+          return res
+        });
+    };
+  
+
