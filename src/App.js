5e037857a1c213100dbc4ecf611115f5bafd7174
unknown <guivecchi12@gmail.com>
2020-07-08 17:26:44 -0600

    Start

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+import fetchShow from "./api/fetchShow";
 
 export default function App() {
   const [show, setShow] = useState(null);
@@ -14,18 +15,14 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
         .then(res => {
+          console.log(res);
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
