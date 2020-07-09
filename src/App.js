fd9cdd02c898ab45b93f8c42dbcbbeb18d38ab9b
Valerie Vazquez <vnvazquez68@gmail.com>
2020-07-08 19:24:00 -0600

    moved api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-
 import { formatSeasons } from "./utils/formatSeasons";
-
 import Episodes from "./components/Episodes";
 import "./styles.css";
-
+import { fetchShow } from "./api/fetchShow";
 export default function App() {
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
@@ -15,20 +13,13 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then((res) => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
