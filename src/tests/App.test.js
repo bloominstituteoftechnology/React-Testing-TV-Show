8d2d10c3c5ca0809365c8eb53e9303afce41a11a
JenVest2020 <jenvest2019@gmail.com>
2020-08-05 18:08:45 -0500

    Episodes tests completed/working on App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { render, fireEvent, wait } from '@testing-library/react';
+import { render, fireEvent, wait, getAllByTestId } from '@testing-library/react';
 import App from '../App';
+import { mockEpisodes } from '../fixtures/mockEpisodes';
+import { fetchShow as mockFetchShow } from '../api/fetchShow';
+
+jest.mock('../api/fetchShow');
 
 test('renders component', () => {
-    render(<App/>)
+    render(<App />)
+});
+
+test('displays seasons when dropdown is clicked', async() => {
+    mockFetchShow.mockResolvedValueOnce({ data: mockEpisodes });
+    const { getByText, queryAllByTestId } = render(<App />);
+    expect(queryAllByTestId('episodes')).toHaveLength(0);
+
+    const drop = getByText(/fetching data/i);
+    fireEvent.click(drop);
+
+    await wait();
+
+    expect(queryAllByTestId('episodes')).toHaveLength(0);
 })
