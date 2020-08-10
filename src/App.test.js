d13e22d91e2f6530a26af6b4eee2c86510a0100d
meholt <meholt90@gmail.com>
2020-08-09 20:29:47 -0700

    finally got all tests to pass

--- src/App.test.js
+++ src/App.test.js
@@ -3,34 +3,36 @@ import {render, waitFor} from '@testing-library/react';
 import { fetchShow as mockFetchShow } from './api/fetchShow';
 import App from './App';
 
-test('App fetches and renders data from api', () => {
-    // Arrange & Act- render component and set up mock data, rerender w/ data
-    const {queryAllByText, rerender} = render(<App />);
+jest.mock('./api/fetchShow');
 
+test('App fetches and renders data from api', async () => {
+    // Arrange & Act- render component and set up mock data
     // Act - rerender component w/ data
-    // rerender(<App error="" episodes={data} />)
-    
     //Check that there is 1 episode rendered: mini assertion
+
+    mockFetchShow.mockResolvedValueOnce(mockData);
+    const {queryAllByText, rerender} = render(<App />);
     expect(queryAllByText(/fetching data.../i)).toHaveLength(1);
-
-
-
-    
-})
+    await waitFor(() => {
+        expect(queryAllByText(/summary/i)).toHaveLength(1);
+    });    
+});
 
 //mock data
 
 const mockData = {
-    image: { img: "img" },
+    image: { original: "original"},
     name: "name",
-    rating: {average: 5},
+    summary: "<p>summary</p>",
     _embedded: {
         episodes: [{
-            id: "0",
+            id: "1",
+            image: { medium: "medium_image"},
             name: "name",
-            season: 1,
-            number: 1,
-            summary: "<p>Episode Summary</p>",
+            season: 3,
+            number: 2,
+            summary: "<p>Summary</p>",
+            runtime: 20
         }]
     }
-}
+};
--- src/utils/formatSeasons.test.js
+++ src/utils/formatSeasons.test.js
@@ -4,8 +4,11 @@ import {formatSeasons} from './formatSeasons';
 // formatSeasons returns an object and this tests for that
 test('Testing that formatSeasons returns an object', () => {
 
-    
+    const expected = {'Season 1': [{season: 1}]}
+    const seasons = formatSeasons(dummyData);
+
+    expect(seasons).toEqual(expected)
 });
 
 // Dummy Data
-const dummyData = [{season: 1}. {season: 2}, {season: 3}];
+const dummyData = [{season: 1}];
