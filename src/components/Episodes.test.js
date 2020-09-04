3d8082ab3bb7a0a9d13a5f352f023ea0f849ee1c
River Smith <rivyyriv@rivers-mbp.lan>
2020-09-04 16:41:47 -0600

    MVP

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Episodes from './Episodes';
+
+test('Renders the main image from API', () => {
+  const {} = render(
+    <Episodes episodes={{}}/>
+  );
+}) 
