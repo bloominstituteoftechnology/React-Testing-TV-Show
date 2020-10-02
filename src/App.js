0aaefc52fb569e14565e11f2e2185bc3f43119cc
Daniel Terry <daniel.terry2121@gmail.com>
2020-10-01 23:27:09 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
+import { fetchShow } from './api/fetchShow'
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
@@ -15,17 +15,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow()
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
@@ -43,6 +37,7 @@ export default function App() {
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
+        data-testid='dropdown'
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
         placeholder="Select an option"
--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -5,7 +5,7 @@ export default function Episodes(props) {
   return (
     <div className="episodes">
       {props.episodes.map(e => (
-        <div className="episode" key={e.id}>
+        <div className="episode" data-testid='episode' key={e.id}>
           {e.image && (
             <img className="episode-image" src={e.image.medium} alt={e.name} />
           )}
