b404a748c4617fb49dd3dc0eaf730086f4085ab6
brandononeal <baoneal95@gmail.com>
2020-10-12 17:47:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
+import fetchShow from "./api/fetchShow";
 import "./styles.css";
 
 export default function App() {
@@ -15,20 +15,13 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then((res) => {
+      setShow(res);
+      setSeasons(formatSeasons(res._embedded.episodes));
+    });
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
