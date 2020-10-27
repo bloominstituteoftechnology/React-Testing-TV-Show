2385fd33f4832810ec00f225dcb59d2e12ca88da
Terry Edwards <terryedwardsjr113@gmail.com>
2020-10-27 16:39:55 -0400

    moved the async call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
@@ -8,6 +8,8 @@ import { formatSeasons } from "./utils/formatSeasons";
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
+import {fetchShow} from './api/fetchShow';
+
 export default function App() {
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
@@ -15,18 +17,23 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
-  }, []);
+    fetchShow()
+      .then(res=>{
+        // console.log(res)
+        setShow(res);
+        setSeasons(formatSeasons(res._embedded.episodes))
+      })},[])
+      // axios
+      //   .get(
+      //     "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      //   )
+      //   .then(res => {
+      //     setShow(res.data);
+      //     setSeasons(formatSeasons(res.data._embedded.episodes));
+      //   });
+    // };
+    // fetchShow();
+  // }, []);
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
---
+++ src/api/fetchShow.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+export const fetchShow = () => {
+   return axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => { return res.data});
+  };
