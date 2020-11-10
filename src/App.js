91f94975f22827560041600525cefd01ee7d9007
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-11-10 09:34:53 -0500

    Axios moved to Component

--- src/App.js
+++ src/App.js
@@ -7,25 +7,22 @@ import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
 import "./styles.css";
+import { fetchShow } from './api/fetchShow';
 
 export default function App() {
-  const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
+  const [show, setShow] = useState(null);
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    
+    fetchShow()
+      .then(res => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+      });
+    
   }, []);
 
   const handleSelect = e => {
