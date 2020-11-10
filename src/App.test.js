bf5a0ee236429927c1c5ccb9f065fe0b009f871b
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-11-10 10:37:44 -0500

    Forgot something, now ddone

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from './App';
-
+import { mockData } from './utils/mockData';
 import { fetchShow as mockFetchShow } from './api/fetchShow';
 jest.mock('./api/fetchShow');
 
-const showData = [data._embedded];
 
 
-test('render without errors', async () => {
-    mockFetchShow.mockResolvedValueOnce(showData);
-    render(<App />);
-    
+describe("App tests", () => {
+    test('render without errors', () => {
+        mockFetchShow.mockResolvedValueOnce(mockData);
+        render(<App />);
+    });
+    test('renders witht he dropdown menu', async () => {
+        mockFetchShow.mockResolvedValueOnce(mockData);
+        render(<App />);
+
+        const dropdown = await screen.findByText(/select a season/i);
+        expect(dropdown).toBeTruthy();
+    });
 });
