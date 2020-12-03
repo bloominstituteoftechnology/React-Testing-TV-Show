776078d008deb384a638091227a502b8e3f45641
Ryan Clark <rbclark244@gmail.com>
2020-12-02 23:43:25 -0500

    mock data made

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,18 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-import Episodes from "./components/Episodes";
-
-test("renders app"),
-  () => {
-    render(<App />);
-  };
-
-// test("renders season images"),
-//   () => {
-//     const { getAllByTestId, rerender } = render(<Episodes images={[]} />);
-//     expect(getAllByTestId(/episodes/i)).toHaveLength(0);
-//   };
