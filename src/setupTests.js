91e8cac8172a369c0e019f8563520b30f6b340f9
Ryan Clark <rbclark244@gmail.com>
2020-12-02 22:08:00 -0500

    finished api transfer

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render } from "@testing-library/react";
+import Episodes from "./components/Episodes";
+import "@testing-library/jest-dom/extend-expect";
+
+test("renders season images"),
+  () => {
+    const { getAllByTestId, rerender } = render(<Episodes images={[]} />);
+    expect(getAllByTestId(/episodes/i)).toHaveLength(0);
+  };
