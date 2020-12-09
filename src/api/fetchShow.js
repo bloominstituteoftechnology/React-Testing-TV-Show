c13f492246ad75d6bb31f3a0f921000b3a1527c4
David Gold <davidgold@hey.com>
2020-12-08 20:12:19 -0500

    Created a separate API directory and moved our async func to a separate file within it

---
+++ src/api/fetchShow.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const fetchShow = () => {
+  axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then((res) => {
+      return res;
+    });
+};
