ab9257e1abd684f02cdb808c1731eb5b3b9a77a8
Brittany Canty <bcanty92@gmail.com>
2021-01-13 18:23:47 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
+import { fetchShow } from './api/fetchShow';
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -15,17 +16,12 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow()
+    .then(res => {
+      // console.log(res);
+      setShow(res);
+      setSeasons(formatSeasons(res._embedded.episodes));
+    })
   }, []);
 
   const handleSelect = e => {
@@ -35,7 +31,7 @@ export default function App() {
   if (!show) {
     return <h2>Fetching data...</h2>;
   }
-
+  
   return (
     <div className="App">
       <img className="poster-img" src={show.image.original} alt={show.name} />
