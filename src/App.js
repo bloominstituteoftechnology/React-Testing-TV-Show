62aa205cdcb081cd8d5d9d3c333dbc481353e9bf
unknown <gradybollas@gmail.com>
2021-01-25 15:17:12 -0800

    two test file needed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Dropdown from "react-dropdown";
-import parse from "html-react-parser";
+import React, { useState, useEffect } from 'react';
+import Dropdown from 'react-dropdown';
+import parse from 'html-react-parser';
 
-import { formatSeasons } from "./utils/formatSeasons";
+import { formatSeasons } from './utils/formatSeasons';
+import { fetchShow } from './api/fetchShow';
 
-import Episodes from "./components/Episodes";
-import "./styles.css";
+import Episodes from './components/Episodes';
+import './styles.css';
 
 export default function App() {
   const [show, setShow] = useState(null);
   const [seasons, setSeasons] = useState([]);
-  const [selectedSeason, setSelectedSeason] = useState("");
+  const [selectedSeason, setSelectedSeason] = useState('');
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow().then(res => {
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
   }, []);
 
   const handleSelect = e => {
@@ -37,15 +30,15 @@ export default function App() {
   }
 
   return (
-    <div className="App">
-      <img className="poster-img" src={show.image.original} alt={show.name} />
+    <div className='App'>
+      <img className='poster-img' src={show.image.original} alt={show.name} />
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
         options={Object.keys(seasons)}
         onChange={handleSelect}
-        value={selectedSeason || "Select a season"}
-        placeholder="Select an option"
+        value={selectedSeason || 'Select a season'}
+        placeholder='Select an option'
       />
       <Episodes episodes={episodes} />
     </div>
