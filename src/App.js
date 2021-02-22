6bd6d232b6ac28240d4034f6652c31f60254eba4
blackcatwizard <55123268+bigbootygoblins@users.noreply.github.com>
2021-02-22 15:47:52 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
-
+import { fetchShow } from './api/fetchshow'
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -15,17 +15,15 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow()
+      .then(res => {
+        setShow(res.data)
+        setSeasons(formatSeasons(res.data_embedded.episodes))
+      })
+      .catch(err => {
+        setShow(false)
+        setSeasons(err.message)
+      })
   }, []);
 
   const handleSelect = e => {
