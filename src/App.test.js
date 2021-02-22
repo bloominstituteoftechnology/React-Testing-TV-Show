cf01f1599e42f7e0209a2f580ad207cb0bf89a89
Daniel Wright <daniel.wright1207@gmail.com>
2021-02-22 12:50:16 -0700

    final after flex

--- src/App.test.js
+++ src/App.test.js
@@ -17,18 +17,4 @@ test("user can select a season", async () => {
   const season1 = await screen.findByText(/season 1/i);
   expect(season1).toBeInTheDocument();
   userEvent.click(season1);
-  // await waitFor(() => {
-  //     expect(screen.)
-  // } )
-});
-test("renders data", async () => {
-  const fakeData = jest.fn();
-  /// arrange render app
-  render(<App />);
-  /// act get the data from api when select season
-  const Dropdown = await screen.findByText(/Select a season/i);
-  //   console.log(Dropdown);
-  userEvent.click(Dropdown);
-  ///should get list of episodes for season one
-  expect();
 });
