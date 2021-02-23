4a475f6da7c0b630531095731320a3419d983596
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-23 16:36:45 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
   useEffect(() => {
     fetchShow()
     .then((res) => {
+      console.log(res.data)
     setShow(res.data);
   setSeasons(formatSeasons(res.data._embedded.episodes));
 
