fb5f1ea66bd699e556edc2845e49050ecd9561bd
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-23 06:20:37 -0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,24 @@
 import React from "react";
-import {render,screen, waitFor} from "@testing-library/react"
+import {getByTitle, render,screen, wait, waitFor} from "@testing-library/react"
 import App from "./App"
 
 test ("app renders without errors",  () => {
     render(<App />)
 })
 
-test ("image appears on the screen",async () => {
-    render(<App />)
+test ("image,H1 appears on the screen",async () => {
+ const{debug} =  render(<App />)
+
+ const h1 = waitFor(() =>expect(screen.findByTitle("Stranger Things")))
+
+ const image = waitFor(() => expect( screen.getByTestId(/image/i)))
+ const paragraph = waitFor(() => expect(screen.getByRole("paragraph")))
 
 
-    
+  expect(image).toBeTruthy()
+  expect(h1).toBeTruthy()
+  expect(paragraph).toBeTruthy()
 
- await waitFor(() => expect( screen.getByTestId(/image/i)))
 
 })
 
